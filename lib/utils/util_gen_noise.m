da64a31f5759eeaf21a497dea0eba9251b5cf132
Taylor <cc2040@hw.ac.uk>
2024-08-28 09:41:15 +0100

    updated for correct sampling pattern

--- lib/utils/util_gen_noise.m
+++ lib/utils/util_gen_noise.m
@@ -17,16 +17,17 @@ function [tau, noise, gdth_img, vis, param_noise] = util_gen_noise(vis_op, adjoi
 
     if param_noise.expo_gdth
         % dynamic range of the ground truth image
+        pattern = '(?<=uv_)\d+';
+        id = regexp(path_uv_data, pattern, 'match');
+        seed = str2num(id{1});
+        rng(seed, 'twister');
         log_sigma = rand() * (log10(1e-3) - log10(2e-6)) + log10(2e-6);
         sigma = 10^log_sigma;
         param_noise.targetDynamicRange = 1/sigma;
         if param_general.sigma0 > 0
             % Exponentiation of the ground truth image
-            pattern = '(?<=_id_)\d+(?=_dt_)';
-            id = regexp(path_uv_data, pattern, 'match');
-            seed = str2num(id{1});
-            rng(seed, 'twister');
             expo_factor = util_solve_expo_factor(param_general.sigma0, sigma);
+            fprintf('\nINFO: exponentiation factor: %g', expo_factor);
             fprintf('\nINFO: target dyanmic range set to %g', param_noise.targetDynamicRange);
             gdth_img = util_expo_im(gdth_img, expo_factor);
         end
@@ -41,7 +42,7 @@ function [tau, noise, gdth_img, vis, param_noise] = util_gen_noise(vis_op, adjoi
             fprintf("\ngenerate noise (noise level commensurate of the target dynamic range) .. ")
 
             targetDynamicRange = param_noise.targetDynamicRange;
-        
+
             % include weights in the measurement op.
             measop_1 = @(x) (nWimag.*vis_op(x));
             adjoint_measop_1 = @(x) (adjoint_vis_op(nWimag.*x));
@@ -56,7 +57,7 @@ function [tau, noise, gdth_img, vis, param_noise] = util_gen_noise(vis_op, adjoi
 
             % noise standard deviation heuristic
             tau  = sqrt(2 * measopSpectralNorm_1) / targetDynamicRange /eta_correction;
-            
+
             % noise realization(mean-0; std-tau)
             noise = tau * (randn(nvis,1) + 1i * randn(nvis,1))./sqrt(2);
 
