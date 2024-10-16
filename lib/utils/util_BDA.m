5bcc84f38f4cfd6a02fc51c14b9b1627a01fd04d
Taylor Chu <cc2040@hw.ac.uk>
2024-10-16 14:17:35 +0100

    updated BDA implementation

--- lib/utils/util_BDA.m
+++ lib/utils/util_BDA.m
@@ -1,31 +1,74 @@
-[I_s, W_bda] = util_BDA(path_uv_data)
+function [I_s, W_bda] = util_BDA(path_uv_data)
     %% extract the coordinates
     load(path_uv_data, 'u_ab', 'v_ab'); %, 'w_ab');
+    nTimeSamples = size(u_ab, 1);
+    na = 64;
+    % TODO: take u and v from param_uv instead of reading from file
     u = u_ab(:);
     v = v_ab(:);
-    % w = w_ab(:);
-
-    % convert unit to km
-    u = u / 1000;
-    v = v / 1000;
-    % w = w / 1000;
 
     baselines = sqrt(u.^2 + v.^2);
-    da_baselines = [80, 40, 30, 20, 15, 10, 7.5, 5, 3.75, 2.5, 1.875, 1.25, 0.9375, 0.625, 0.5625, 0.375, 0.28125, 0];
-    bda_baselines = flip(bda_baselines);
-    bda_avg = [1, 2, 3, 4, 6, 8, 12, 16, 24, 32, 48, 64, 96, 128, 192, 256, 384, 512];
-    bda_avg = flip(bda_avg);
+    baseline_min = min(baselines(baselines > 0));
 
-    bins = discretize(baselines, bda_baselines);
+    baseline_edges = [max(baselines)];
+    num_pt_avg = [1];
+    while true
+        baseline_edge_new = baseline_edges(end)/2;
+        if baseline_edge_new < baseline_min
+            break
+        else
+            baseline_edges = [baseline_edges baseline_edge_new];
+            num_pt_avg = [num_pt_avg num_pt_avg(end) * 2];
+        end
+    end
+    baseline_edges = [baseline_edges 0];
+    baseline_edges = flip(baseline_edges);
+    num_pt_avg = flip(num_pt_avg);
+    num_pt_avg = num_pt_avg(:);
+    bins = discretize(baselines, baseline_edges);
 
-    num_bins = length(unique(bins));
-
-    I_s = zeros([num_bins, length(baselines)]);
-    for i = 1:length(bins)
-        I_s(bins(i), i) = 1;
+    max_num_per_bin = zeros(length(num_pt_avg), 1);
+    for i = 1:nTimeSamples
+        baselines_b = sqrt(u_ab(i,:,:).^2 + v_ab(i,:,:).^2);
+        bins_b = discretize(baselines_b, baseline_edges);
+        [C, ~, ic] = unique(bins_b);
+        counts = accumarray(ic, 1);
+        num_pt_per_bin = ceil(counts ./ num_pt_avg(C));
+        max_num_per_bin(C) = max_num_per_bin(C) + num_pt_per_bin;
     end
 
-    W_bda = 1 ./ (I_s * I_s');
+    M = sum(max_num_per_bin);
+    I_s = sparse(M, length(baselines));
+    M_count_start = cumsum(max_num_per_bin);
+    M_count = zeros(length(max_num_per_bin), 1);
+    for i = 1:nTimeSamples
+        baselines_b = sqrt(squeeze(u_ab(i,:,:)).^2 + squeeze(v_ab(i,:,:)).^2);
+        bins_b = discretize(baselines_b, baseline_edges);
+        [C, ~, ic] = unique(bins_b);
+        counts = accumarray(ic, 1);
+        num_pt_per_bin = ceil(counts ./ num_pt_avg(C));
+        counter = 0;
+        for j = 1:length(C)
+            if num_pt_per_bin(j) == 0
+                continue
+            else
+                bin_b_j_idx = find(bins_b == C(j));
+                for k = 1:num_pt_per_bin(j)
+                    M_count_start_j = M_count_start(C(j)) + M_count(C(j));
+                    k_start = num_pt_avg(C(j)) * (k - 1) + 1;
+                    if num_pt_avg(C(j)) * k > length(bin_b_j_idx)
+                        k_stop = length(bin_b_j_idx);
+                    else
+                        k_stop = num_pt_avg(C(j)) * k;
+                    end
+                    counter = counter + (k_stop - k_start);
+                    I_s(M_count_start_j, na^2 * (i - 1) + bin_b_j_idx(k_start:k_stop)) = 1;
+                    M_count(C(j)) = M_count(C(j)) + 1;
+                end
+            end
+        end
+    end
+    W_bda = 1 ./ full(diag(I_s * I_s'));
     W_bda(isinf(W_bda)) = 0;
 
 end
